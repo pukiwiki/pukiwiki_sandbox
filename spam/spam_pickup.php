3a02a7a8302624d85c6b431fe51fc3f5d1efdec0
henoheno <henoheno>
2007-01-21 23:51:38 +0900

    Simplify

--- spam/spam_pickup.php
+++ spam/spam_pickup.php
@@ -1,7 +1,7 @@
 <?php
-// $Id: spam_pickup.php,v 1.36 2007/01/03 14:14:20 henoheno Exp $
+// $Id: spam_pickup.php,v 1.37 2007/01/21 14:51:38 henoheno Exp $
 // Concept-work of spam-uri metrics
-// Copyright (C) 2006 PukiWiki Developers Team
+// Copyright (C) 2006-2007 PukiWiki Developers Team
 // License: GPL v2 or (at your option) any later version
 
 error_reporting(E_ALL); // Debug purpose
@@ -25,46 +25,45 @@ function recursive_map($func, $array)
 
 function show_form($string, $asap = FALSE, $progress = TRUE, $pickup = TRUE)
 {
-	if ($asap) {
-		$asap = ' checked';
-	} else {
-		$asap = '';
-	}
-	if ($pickup) {
-		$pickup = ' checked';
-	} else {
-		$pickup = '';
-	}
-	if ($progress) {
-		$progress = ' checked';
-	} else {
-		$progress = '';
-	}
-	$base = basename(__FILE__);
-	$string = htmlspecialchars($string);
-	print <<< EOF
+	$string   = htmlspecialchars($string);
+	$asap     = $asap     ? ' checked' : '';
+	$progress = $progress ? ' checked' : '';
+	$pickup   = $pickup   ? ' checked' : '';
+	$base     = basename(__FILE__);
+
+	return <<< EOF
 <form action="$base" method="post">
+
 	<textarea name="msg" rows="8" cols="80">$string</textarea><br />
+
 	<input type="checkbox" name="asap"   id="asap"   value="on"$asap>
 	<label for="asap">asap</label><br />
+
 	<input type="checkbox" name="progress" id="progress" value="on"$progress>
 	<label for="progress">Show \$progress</label><br />
+
 	<input type="checkbox" name="pickup" id="pickup" value="on"$pickup>
 	<label for="pickup">Show pickuped URIs</label><br />
+
 	<input type="submit" name="write" value="Submit" />
+
 </form>
-<br/>
 EOF;
+
 }
 
 
 // ---- Show form and result
 echo basename(__FILE__) . '<br />';
+
 $msg    = isset($_POST['msg'])      ? $_POST['msg'] : '';
 $asap   = isset($_POST['asap'])     ? TRUE : FALSE;
 $prog   = isset($_POST['progress']) ? TRUE : FALSE;
 $pickup = isset($_POST['pickup'])   ? TRUE : FALSE;
-show_form($msg, $asap, $prog, $pickup);
+
+echo show_form($msg, $asap, $prog, $pickup);
+echo '<br/>';
+
 echo '<pre>';
 
 $method = check_uri_spam_method();
@@ -108,6 +107,7 @@ if ($pickup) {
 	$results = uri_array_normalize($results, TRUE);
 	var_dump('$results', $results);
 }
+
 echo '</pre>';
 
 ?>
