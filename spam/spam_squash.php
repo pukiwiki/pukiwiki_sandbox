5dc28dd3433f728da83978bd00bac2f296a93fb5
henoheno <henoheno>
2008-12-28 17:35:45 +0900

    (none)

---
+++ spam/spam_squash.php
@@ -0,0 +1,13 @@
+<?php
+// $Id: spam_squash.php,v 1.1 2008/12/28 08:35:45 henoheno Exp $
+// Copyright (C) 2006-2007 PukiWiki Developers Team
+// License: GPL v2 or (at your option) any later version
+//
+// Functions for Concept-work of spam-uri metrics
+
+// ---------------------
+// Squash object for anti-spamming <=> metrics
+
+
+
+?>
